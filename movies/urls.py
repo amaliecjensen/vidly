7afd780d4debe35e1e9ab15e0ad54e61e7efcabc
amaliecjensen <amaliecolstrup@icloud.com>
2026-01-23 11:32:31 +0100

    init

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='movies_index'),
-    path('<movie_id>', views.detail, name='movies_detail')
+    path('<int:movie_id>', views.detail, name='movies_detail'),
+    path('api/movies/', views.api_movies, name='api_movies'),
+    path('api/movies/<int:movie_id>',
+         views.api_movie_detail, name='api_movie_detail')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 
@@ -9,4 +9,22 @@ def index(request):
 
 
 def detail(request, movie_id):
-    return HttpResponse(movie_id)
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
+
+
+def api_movies(request):
+    movies = list(Movie.objects.all().values())
+    return JsonResponse({'movies': movies})
+
+
+def api_movie_detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    data = {
+        'title': movie.title,
+        'release_year': movie.release_year,
+        'number_in_stock': movie.number_in_stock,
+        'daily_rate': movie.daily_rate,
+        'genre': movie.genre.name
+    }
+    return JsonResponse(data)
