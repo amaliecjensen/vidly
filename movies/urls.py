41e1f6e7b8397dba44a822563db500c4eee27517
amaliecjensen <amaliecolstrup@icloud.com>
2026-01-23 11:27:31 +0100

    init

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
-# movies/
-# movies/1/details
-
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='movies_index'),
+    path('<movie_id>', views.detail, name='movies_detail')
 ]
